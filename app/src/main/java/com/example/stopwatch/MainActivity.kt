dd4cc1acd4e191644ff75c7e255c62b00d8958f7
per2 <per2@compsci-12.spusd.org>
2023-09-06 09:13:22 -0700

    Initial commit

---
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.stopwatch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity()
+{
+
+    //kotlin constants: companion objects
+    companion object
+    {
+        val TAG = "MainActivity"
+    }
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "onCreate: KOTLIN AND ANDROID STUDIO ARE INEFFICIENT AND LAME!")
+    }
+
+    override fun onStart()
+    {
+        super.onStart()
+    }
+}
