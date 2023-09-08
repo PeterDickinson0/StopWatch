1a18760f1dd001ed4e3bf5b0dbefba597605e100
PeterDickinson <peter.dickinson06@gmail.com>
2023-09-08 09:04:58 -0700

    Added logs to all stages of lifecycle

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.stopwatch
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -13,6 +14,7 @@ class MainActivity : AppCompatActivity()
         val TAG = "MainActivity"
         val USELESSPI = 3
     }
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
@@ -21,33 +23,45 @@ class MainActivity : AppCompatActivity()
         Log.d(TAG, "onCreate: KOTLIN AND ANDROID STUDIO ARE INEFFICIENT AND LAME!")
     }
 
+    override fun onConfigurationChanged(newConfig: Configuration)
+    {
+        super.onConfigurationChanged(newConfig)
+        Log.d(TAG,"onCofigurationChanged: Screen Rotated")
+    }
+
     override fun onStart()
     {
         super.onStart()
+        Log.d(TAG, "onStart: started")
     }
 
     override fun onResume()
     {
         super.onResume()
+        Log.d(TAG, "onResume: resumed")
     }
 
     override fun onRestart()
     {
         super.onRestart()
+        Log.d(TAG, "onRestart: restarted")
     }
 
     override fun onPause()
     {
+        Log.d(TAG, "onPause: paused")
         super.onPause()
     }
 
     override fun onStop()
     {
         super.onStop()
+        Log.d(TAG, "onStop: stopped")
     }
 
     override fun onDestroy()
     {
         super.onDestroy()
+        Log.d(TAG, "onDestroy: destroyed")
     }
 }
